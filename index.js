ef2afd73d301a604abc8814a6db7ad02b8d026eb
Artem Oleksandrenko <artem2008.zp@gmail.com>
2024-07-13 17:14:27 +0300

    Update JS code

--- index.js
+++ index.js
@@ -1065,3 +1065,61 @@ switch (format) {
         console.log('Unnamed file');
         break;
 }
+
+const number = 301;
+console.log(number, typeof number);
+const numToStr = String(number);
+console.log(numToStr, typeof numToStr);
+const arrStr = numToStr.split('');
+console.log(arrStr, typeof arrStr);
+const arrNums = arrStr.map((elem) => Number(elem));
+const summa = arrNums.reduce((accumulator, elem) => accumulator + elem);
+console.log(summa, typeof summa);
+
+const result = String(number)
+    .split('')
+    .map((elem) => Number(elem))
+    .reduce((accumulator, elem) => accumulator + elem);
+const minNumber = Number(String(number).split('').sort().at(0))
+const maxNumber = Number(String(number).split('').sort().at(-1))
+console.log(minNumber);
+console.log(maxNumber);
+
+const newNumbers = [5, 172, 1, 10];
+const numbersSorted = newNumbers.toSorted((a, b) => a - b);
+const numbersSortedReverse = numbersSorted.toReversed();
+console.log(newNumbers);
+console.log(numbersSorted);
+console.log(numbersSortedReverse);
+
+const products = [
+    {id: 1, title: 'milk', price: 30.45, amount: 50, category: 'milk & cheeses'},
+    {id: 3, title: 'chees', price: 330.75, amount: 5, category: 'milk & cheeses'},
+    {id: 2, title: 'beef', price: 250.15, amount: 7, category: 'meat'},
+    {id: 8, title: 'chicken', price: 130.20, amount: 100, category: 'meat'},
+    {id: 7, title: 'cookie', price: 33.00, amount: 65, category: 'cookie & bread'},
+    {id: 27, title: 'bread', price: 20.50, amount: 14, category: 'cookie & bread'},
+    {id: 6, title: 'yogurt', price: 30.45, amount: 23, category: 'milk & cheeses'},
+    {id: 24, title: 'fish', price: 185.60, amount: 62, category: 'meat'}
+]
+console.table(products);
+const sortedProductByPrice = products.toSorted((a, b) => a.price - b.price);
+const productWithMinPrice = sortedProductByPrice.at(0);
+console.table(sortedProductByPrice);
+console.table(productWithMinPrice);
+const sortedProductByTitle = products.toSorted((a, b) => {
+    if (a.title === b.title) {
+        return 0;
+    }
+    if (a.title > b.title) {
+        return 1;
+    }
+    if (a.title < b.title) {
+        return -1;
+    }
+})
+console.table(sortedProductByTitle);
+const meatProducts = products.filter((elem) => elem.category === 'meat');
+console.table(meatProducts);
+const productsFilter = products.filter((elem) => elem.amount < 30);
+console.table(productsFilter);
